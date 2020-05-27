c8b0ac2915a6361f99558d0c871a36b5d0b403d3
Eduardo <eduardorodalm95@gmail.com>
2020-05-27 17:13:42 +0100

    obvtransf ready

--- raspy64/src/components/raspadinha/raspadinha.vue
+++ raspy64/src/components/raspadinha/raspadinha.vue
@@ -3,108 +3,95 @@
     <v-row>
       <v-col></v-col>
       <v-col>
-        <p class="text-center headline font-weight-bold">
-          Try your luck and click in the button below!
-        </p>
+        <p
+          class="text-center headline font-weight-bold"
+        >Try your luck and click in the button below!</p>
         <v-col align="center">
           <v-dialog v-model="dialog" persistent width="550">
             <template v-slot:activator="{ on }">
               <div v-if="!userloggedIn">
-                <v-btn class="w3-hover-white" v-on="on" disabled
-                  >Lets win fake money!</v-btn
-                >
+                <v-btn class="w3-hover-white" v-on="on" disabled>Lets win fake money!</v-btn>
               </div>
 
               <div v-else>
-                <v-btn class="w3-hover-white" v-on="on" @click="getrasp()"
-                  >Lets win fake money!</v-btn
-                >
+                <v-text-field v-model="b" :rules="brules" label="Choose the value 0 or 1" required></v-text-field>
+
+                <v-text-field
+                  v-model="k"
+                  :rules="krules"
+                  label="Choose a number bigger than 0"
+                  required
+                ></v-text-field>
+                <div v-if="b != -1 && k!=-1">
+                  <v-btn class="w3-hover-white" v-on="on" @click="getrasp()">Lets win fake money!</v-btn>
+                </div>
+                <div v-else>
+                  <v-btn class="w3-hover-white" v-on="on" disabled>Lets win fake money!</v-btn>
+                </div>
               </div>
             </template>
             <div v-if="!letsroll">
               <v-card>
-                <v-card-title class="headline orange darken-1" primary-title
-                  >Sorry!</v-card-title
-                >
+                <v-card-title class="headline orange darken-1" primary-title>Sorry!</v-card-title>
 
-                <v-card-text
-                  >Not yet you still have to wait
-                  {{ this.$store.state.timeleft }}.</v-card-text
-                >
+                <v-card-text>
+                  Not yet you still have to wait
+                  {{ this.$store.state.timeleft }}.
+                </v-card-text>
                 <v-divider></v-divider>
 
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn
-                    color="orange"
-                    text
-                    @click="(dialog = false), aud3.play()"
-                    >Okey okey!</v-btn
-                  >
+                  <v-btn color="orange" text @click="(dialog = false), aud3.play()">Okey okey!</v-btn>
                 </v-card-actions>
               </v-card>
             </div>
             <div v-else>
               <div v-if="victorious">
                 <v-card>
-                  <v-card-title class="headline orange darken-1" primary-title
-                    >You Actually WON something!!!</v-card-title
-                  >
+                  <v-card-title
+                    class="headline orange darken-1"
+                    primary-title
+                  >You Actually WON something!!!</v-card-title>
 
                   <v-card-text>
-                    <v-img
-                      src="../../../public/img/icons/happy-dog.gif"
-                      lazy-src
-                      contain
-                    ></v-img>
+                    <v-img src="../../../public/img/icons/happy-dog.gif" lazy-src contain></v-img>
                   </v-card-text>
                   <v-divider></v-divider>
 
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="orange" text @click="dialog = false"
-                      >Thanks Man!</v-btn
-                    >
+                    <v-btn color="orange" text @click="dialog = false">Thanks Man!</v-btn>
                   </v-card-actions>
                 </v-card>
               </div>
               <div v-else>
                 <v-card>
-                  <v-card-title class="headline orange darken-1" primary-title
-                    >Better luck next time!</v-card-title
-                  >
+                  <v-card-title
+                    class="headline orange darken-1"
+                    primary-title
+                  >Better luck next time!</v-card-title>
 
                   <v-card-text>
-                    <v-img
-                      src="../../../public/img/icons/africacoffin.gif"
-                      lazy-src
-                      contain
-                    ></v-img>
+                    <v-img src="../../../public/img/icons/africacoffin.gif" lazy-src contain></v-img>
                   </v-card-text>
                   <v-divider></v-divider>
 
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="orange" text @click="dialog = false"
-                      >Never Surrender!</v-btn
-                    >
+                    <v-btn color="orange" text @click="dialog = false">Never Surrender!</v-btn>
                   </v-card-actions>
                 </v-card>
               </div>
             </div>
           </v-dialog>
         </v-col>
-        <v-img
-          src="../../../public/img/icons/take-my-money.gif"
-          style="width:700px"
-        >
+        <v-img src="../../../public/img/icons/take-my-money.gif" style="width:700px">
           <div class="fill-height bottom-gradient"></div>
         </v-img>
         <v-alert type="warning">
           <h5 class>Warning!</h5>
-          <h6 class="w3-opacity">
-            This might be click bait continue at your own risk.
-          </h6>
+          <h6 class="w3-opacity">This might be click bait continue at your own risk.</h6>
         </v-alert>
       </v-col>
       <v-col></v-col>
@@ -125,13 +112,23 @@ export default {
       aud3: new Audio(require("../../../public/audio/yara.ogg")),
       aud2: new Audio(require("../../../public/audio/africacofin.mp3")),
       letsroll: false,
+      b: -1,
+      k: -1,
+      brules: [
+        v => !!v || "Please enter the value!",
+        v => v == 0 || v == 1 || "Number must be equal to zero or one!"
+      ],
+      krules: [
+        v => !!v || "Please enter the value!",
+        v => v > 0 || "Number must be bigger than zero!"
+      ]
     };
   },
   computed: {
     userloggedIn() {
       console.log(this.$store.getters.userloggedIn);
       return this.$store.getters.userloggedIn;
-    },
+    }
   },
   components: {},
   methods: {
@@ -175,14 +172,22 @@ export default {
           .database()
           .ref("Users/" + this.$store.state.user)
           .update({
-            raspadinha: cryptico.encrypt(0, pk).cipher,
+            raspadinha: cryptico.encrypt(0, pk).cipher
           });
         this.letsroll = true;
       } else {
         this.letsroll = false;
       }
       this.letsroll = true;
-      const val = await this.$store.dispatch("getrasp");
+
+      console.log(this.b);
+      console.log(this.k);
+
+      const bk = {
+        b: Math.round(this.b),
+        k: Math.round(this.k)
+      };
+      const val = await this.$store.dispatch("getrasp", bk);
       this.victorious = val;
       if (this.victorious && this.letsroll) {
         //this.aud1.play();
@@ -197,8 +202,10 @@ export default {
           d.getHours() * 60 + d.getMinutes()
         );
       }
-    },
+      this.b = -1;
+      this.k = -1;
+    }
   },
-  mounted() {},
+  mounted() {}
 };
 </script>
